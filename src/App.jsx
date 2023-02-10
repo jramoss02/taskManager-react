a569bbab8c68f9f466aaf8e28e110f00298207a7
José Ramos <02.jrsramos@gmail.com>
2023-02-10 11:27:37 -0300

    confirm-task

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,43 @@
 import React, { useState } from "react";
 import Tasks from "./components/Tasks";
 
+import {v4 as uuidv4} from 'uuid';
+
 import "./App.css";
-import AddTask from "./components/AddTassk";
+import AddTask from "./components/AddTask";
 
 const App = () => {
   const [tasks, setTasks] = useState([
     {
       id: '1',
-      title: 'Estudar programação',
+      title: 'Teste01',
       completed: false,
     },
 
     {
       id: '2',
-      title: 'Ler livros',
+      title: 'Teste02',
       completed: true,
     },
   ]); 
+
+  const handleTaskAddiion = (taskTitle) => {
+    const newTasks = [...tasks,
+      {
+        title: taskTitle,
+        id: uuidv4(),
+        completed: false,
+      },
+    ];
+
+    setTasks(newTasks);
+  }
   
   return (
     <>
       <div className="container">
-        <AddTask/>
-        <Tasks tasks={tasks}/>  
+        <AddTask handleTaskAddition={handleTaskAddiion}/>
+        <Tasks tasks={tasks}/>
       </div> 
     </>
   );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import "./Button.css";
+
 const Button = ({children, onClick}) => {
-    return <button onClick={onClick} className='button'>{children}</button>;
-}
+    return (
+        <button onClick={onClick} className='button'>
+            {children}
+        </button>
+    );
+};
+
+<Button></Button>
 
 export default Button;
